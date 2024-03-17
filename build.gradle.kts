b662b115279e2a4c249318b56b7a18d0b88e7426
Eldar <eldar.tim@gmail.com>
2024-03-17 05:25:06 +0300

    build: update lib versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ group = "de.torsm"
 version = "1.0"
 
 plugins {
-    kotlin("jvm") version "1.9.21"
+    kotlin("jvm") version "1.9.22"
 }
 
 kotlin {
@@ -16,11 +16,11 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    implementation("io.ktor:ktor-network:2.3.6")
-    implementation("ch.qos.logback:logback-classic:1.4.11")
+    implementation("io.ktor:ktor-network:2.3.7")
+    implementation("ch.qos.logback:logback-classic:1.4.14")
     testImplementation(kotlin("test-junit5"))
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.1")
 }
 
 tasks.test {
